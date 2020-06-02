9b660202cab44eda00b88cf871eee026710bcaad
troyschennum <tj.schennum@gmail.com>
2020-06-02 10:35:02 -0500

    react todo assigment done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react"
+
+import Item from "./Todo"
+
+const TodoList = props => {
+    return (
+        <div className="todos">
+            {props.todos.map(item => (
+                <Item toggleItem={props.toggleItem} key={item.id} item={item} />
+            ))}
+            <button className="clear-btn" onClick={props.clearCompleted}>Completed</button>
+        </div>
+    );
+};
+export default TodoList;
